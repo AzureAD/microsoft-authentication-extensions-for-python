dfdb5eca0f38e38cad0b39dbd810c2b3a9e5180d
Martin Strobel <marstr@microsoft.com>
2019-05-09 15:32:48 -0700

    Establish Project File Structure (#10)
    * Establishing most basic file structure.
    
    * Promoting msal_extensions folder in file structure.
    
    * Parse __init__ for version number.
    
    This flow is based on code found here:
    https://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package/39671214#39671214
    
    However the pattern was expanded slightly to handle String Literal Prefixes as defined here:
    https://docs.python.org/3/reference/lexical_analysis.html#grammar-token-stringprefix
    
    * Exclude tests from bundled packages.
    
    * Fixing formatting nit
    
    * Removing "tests" from being a package.
    
    This prevents "find_packages" from picking it up, even if there is no exclusion set.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import re, io
+
+__version__ = re.search(
+    r'__version__\s*=\s*[rRfFuU]{0,2}[\'"]([^\'"]*)[\'"]',
+    io.open('msal_extensions/__init__.py', encoding='utf_8_sig').read()
+    ).group(1)
+
+setup(
+    name='msal-extensions',
+    version=__version__,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+    tests_require=['pytest'],
+)

7c65d7131255e80c3b7f6c7721a83784e019cc2e
Martin Strobel <marstr@microsoft.com>
2019-05-21 13:21:59 -0700

    Add WindowsTokenCache roundtrip test

--- tests/test_windows_backend.py
+++ tests/test_windows_backend.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import pathlib
+import shutil
+import tempfile
 import pytest
 import uuid
 import msal
@@ -7,7 +10,7 @@ import msal
 if not sys.platform.startswith('win'):
     pytest.skip('skipping windows-only tests', allow_module_level=True)
 else:
-    from msal_extensions.windows import WindowsDataProtectionAgent
+    from msal_extensions.windows import WindowsDataProtectionAgent, WindowsTokenCache
 
 
 def test_dpapi_roundtrip_with_entropy():
@@ -67,3 +70,26 @@ def test_read_msal_cache_direct():
     cache.deserialize(raw)
     access_tokens = cache.find(msal.TokenCache.CredentialType.ACCESS_TOKEN)
     assert len(access_tokens) > 0
+
+
+def test_windows_token_cache_roundtrip():
+    client_id = os.getenv('AZURE_CLIENT_ID')
+    client_secret = os.getenv('AZURE_CLIENT_SECRET')
+    if not (client_id and client_secret):
+        pytest.skip('no credentials present to test WindowsTokenCache round-trip with.')
+
+    test_folder = tempfile.mkdtemp(prefix="msal_extension_test_windows_token_cache_roundtrip")
+    cache_file = os.path.join(test_folder, 'msal.cache')
+    try:
+        subject = WindowsTokenCache(cache_location=cache_file)
+        app = msal.ConfidentialClientApplication(
+            client_id=client_id,
+            client_credential=client_secret,
+            token_cache=subject)
+        desired_scopes = ['https://graph.microsoft.com/.default']
+        token1 = app.acquire_token_for_client(scopes=desired_scopes)
+        os.utime(cache_file, None)  # Mock having another process update the cache.
+        token2 = app.acquire_token_silent(scopes=desired_scopes, account=None)
+        assert token1['access_token'] == token2['access_token']
+    finally:
+        shutil.rmtree(test_folder, ignore_errors=True)

3667b2ad71267e46e7bc3a03044b573ad58101af
Martin Strobel <marstr@microsoft.com>
2019-05-22 10:45:09 -0700

    Add TravisCI (#18)
    * Add TravisCI
    
    * Fixing no-newline nit
    
    * Auto-complete error
    
    * Fix pylint syntax
    
    * Remove duplicate linux jobs
    
    * Fix pylint violations
    
    * Updating suppression mechanism
    
    * Fixing Python2 compat bug
    
    * Set distro to xenial
    
    * Fixing non-Linux TravisCI support
    
    I had to follow this guidance:
    https://docs.travis-ci.com/user/languages/python/#running-python-tests-on-multiple-operating-systems
    
    * Add Windows Python2
    
    * Experimentally change chocolately package name
    
    * Fix chocolatey version spec
    
    * Add Windows 3.5 to matrix
    
    * Fix DocString

--- msal_extensions/cache_lock.py
+++ msal_extensions/cache_lock.py
@@ -1,3 +1,4 @@
+"""Provides a mechanism for not competing with other processes interacting with an MSAL cache."""
 import os
 import sys
 import errno
@@ -5,12 +6,13 @@ import portalocker
 
 
 class CrossPlatLock(object):
-    """
-    Offers a mechanism for waiting until another process is finished interacting with a shared resource. This is
-    specifically written to interact with a class of the same name in the .NET extensions library.
+    """Offers a mechanism for waiting until another process is finished interacting with a shared
+    resource. This is specifically written to interact with a class of the same name in the .NET
+    extensions library.
     """
     def __init__(self, lockfile_path):
         self._lockpath = lockfile_path
+        self._fh = None
 
     def __enter__(self):
         pid = os.getpid()
@@ -22,11 +24,10 @@ class CrossPlatLock(object):
     def __exit__(self, *args):
         self._fh.close()
         try:
-            # Attempt to delete the lockfile. In either of the failure cases enumerated below, it is likely that
-            # another process has raced this one and ended up clearing or locking the file for itself.
+            # Attempt to delete the lockfile. In either of the failure cases enumerated below, it is
+            # likely that another process has raced this one and ended up clearing or locking the
+            # file for itself.
             os.remove(self._lockpath)
-        except PermissionError:
-            pass
         except OSError as ex:
-            if ex.errno != errno.ENOENT:
+            if ex.errno != errno.ENOENT and ex.errno != errno.EACCES:
                 raise

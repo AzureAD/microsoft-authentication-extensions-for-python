b8f3f3f34146335a3d2cc5bafa01f13f799b81ea
Martin Strobel <marstr@microsoft.com>
2019-05-30 13:13:34 -0700

    Fix pylint error about unnecessary `elif` statements.

--- msal_extensions/token_cache.py
+++ msal_extensions/token_cache.py
@@ -9,9 +9,11 @@ elif sys.platform.startswith('darwin'):
 def get_protected_token_cache(enforce_encryption=False, **kwargs):
     if sys.platform.startswith('win'):
         return WindowsTokenCache(**kwargs)
-    elif sys.platform.startswith('darwin'):
+
+    if sys.platform.startswith('darwin'):
         return OSXTokenCache(**kwargs)
-    elif enforce_encryption:
+
+    if enforce_encryption:
         raise RuntimeError('no protected token cache for platform {}'.format(sys.platform))
-    else:
-        raise NotImplementedError('No fallback TokenCache is implemented yet.')
+
+    raise NotImplementedError('No fallback TokenCache is implemented yet.')

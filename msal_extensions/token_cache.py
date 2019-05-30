3b124dc22821c2a5d83a94267f0cc1b23d49d7a3
Martin Strobel <marstr@microsoft.com>
2019-05-30 13:28:08 -0700

    Add missing doc-strings as noted by pylint

--- msal_extensions/token_cache.py
+++ msal_extensions/token_cache.py
@@ -1,3 +1,4 @@
+"""Generic functions and types for working with a TokenCache that is not platform specific."""
 import sys
 
 if sys.platform.startswith('win'):
@@ -7,6 +8,15 @@ elif sys.platform.startswith('darwin'):
 
 
 def get_protected_token_cache(enforce_encryption=False, **kwargs):
+    """Detects the current system, and constructs a TokenCache of the appropriate type for the
+    environment in which it's running.
+
+    :param enforce_encryption: When 'True' an error will be raised if there isn't an encrypted
+    option available for the current system. When 'False', a plain-text option will be returned.
+    :param kwargs: Any options that should be passed to the platform-specific constructor of the
+    TokenCache being instantiated by this method.
+    :return: A fully instantiated TokenCache able to encrypt/decrypt tokens on the current system.
+    """
     if sys.platform.startswith('win'):
         return WindowsTokenCache(**kwargs)
 

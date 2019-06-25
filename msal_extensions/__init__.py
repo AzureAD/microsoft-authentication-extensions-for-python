6bc51417162e859eccad7d750d13070555331834
Martin Strobel <marstr@microsoft.com>
2019-06-25 12:08:56 -0700

    Add KeyChain Cache, Plain-Text File Cache, and Cache Factory (#15)
    * Add KeyChain Cache
    
    * Use **kwargs in OSXTokenCache.find
    
    * Fixing pylint errors
    
    * Use CrossPlatLock in OSX Cache
    
    * Adding partially implemented cache MUX
    
    * Overload modify instead of more specific commands
    
    * Squash warnings about duplicate code
    
    * Fix pylint error about unnecessary `elif` statements.
    
    * Fixing line formatting err from pylint
    
    * Add missing doc-strings as noted by pylint
    
    * Revert "Squash warnings about duplicate code"
    
    This reverts commit f8f61bd08492100ebeef3e3213c01e0c38bfb30c.
    
    * Pull duplicated code into single unprotected SerializableTokenCache implementation that can be overriden
    
    * Move all token cache implementations into .token_cache
    
    Previously, implementations lived in their platform specific files. However, this created an import cycle that pylint was complaining about.
    
    * Fixing formatting nit
    
    * Mux on generic form of FileTokenCache as fallback plan
    
    * Removing redundant test
    
    * Fix documentation typo
    
    * Making directory creation call in _mkdir_p recursive.
    
    * Restructuring what occurs in _read/_write and the other modifiers
    
    * Fix kwargs use in constructors
    
    * Fix invalid/inaccurate file mode for agnostic backend
    
    * Add test for plain file token cache.
    
    * Moving error handling out of _read and _write.
    
    This accomplishes a couple of things:
    	- Establishes that _read and _write SHOULD error out if they are unable to do their job. This way different places in code base can choose what is and is not an acceptable place to fail a read or write operation.
    	- Keeps the contract that derived class implementers need to fulfill very simple.
    
    * Suppress pylint error caused by difference in parameters, which is done strategically with kwargs
    
    * Removing duplicated line
    
    * Brining all Keychain Error constructors into the same shape.
    
    * Don't defer failure of OS module loads
    
    * Remove child error-types
    
    * Do token cache switching at import time instead of having a separate factory

--- msal_extensions/__init__.py
+++ msal_extensions/__init__.py
@@ -1,2 +1,11 @@
 """Provides auxiliary functionality to the `msal` package."""
 __version__ = "0.0.1"
+
+import sys
+
+if sys.platform.startswith('win'):
+    from .token_cache import WindowsTokenCache as TokenCache
+elif sys.platform.startswith('darwin'):
+    from .token_cache import OSXTokenCache as TokenCache
+else:
+    from .token_cache import UnencryptedTokenCache as TokenCache

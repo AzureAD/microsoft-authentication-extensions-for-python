c8f4774a8bac627be5c54f90dc4fd5cb6726ff08
Ray Luo <rayluo.mba@gmail.com>
2020-04-03 12:52:15 -0700

    A generic persistence layer, some provides data encryption
    Remove dummy and use Path(...).touch() instead
    
    Document schema_name and attributes
    
    Change getmtime() to time_last_modified()
    
    Rename 2 location into signal_location, with docs
    
    Refactor test cases using pytest.mark.skipif

--- msal_extensions/__init__.py
+++ msal_extensions/__init__.py
@@ -3,6 +3,14 @@ __version__ = "0.1.3"
 
 import sys
 
+from .persistence import (
+    FilePersistence,
+    FilePersistenceWithDataProtection,
+    KeychainPersistence,
+    LibsecretPersistence,
+    )
+from .cache_lock import CrossPlatLock
+
 if sys.platform.startswith('win'):
     from .token_cache import WindowsTokenCache as TokenCache
 elif sys.platform.startswith('darwin'):

da5574f9152d55b639270fb23e387e2e36692f43
Abhidnya <patil.abhidnya@gmail.com>
2020-05-19 12:58:27 -0700

    Experimenting 1

--- tests/test_cache_lock_file_perf.py
+++ tests/test_cache_lock_file_perf.py
@@ -72,7 +72,7 @@ def _run_multiple_processes(no_of_processes, cache_location, sleep_interval):
 
 
 def test_multiple_processes_without_timeout_exception(cache_location):
-    num_of_processes = 50
+    num_of_processes = 20
     sleep_interval = 0
     _run_multiple_processes(num_of_processes, cache_location, sleep_interval)
     _validate_result_in_cache(num_of_processes, cache_location)

e7fd866aa9136fd3a488819396f3d28bf36c088b
Abhidnya <patil.abhidnya@gmail.com>
2020-06-05 12:10:37 -0700

    Changing the format of doc_string

--- tests/cache_file_generator.py
+++ tests/cache_file_generator.py
@@ -1,6 +1,9 @@
 """
+Usage: cache_file_generator.py cache_file_path sleep_interval
+
 This is a console application which is to be used for cross-platform lock performance testing.
-The app will acquire lock for the cache file, log the process id and free the lock
+The app will acquire lock for the cache file, log the process id and then release the lock.
+
 It takes in two arguments - cache file path and the sleep interval.
 The cache file path is the path of cache file.
 The sleep interval is the time in seconds for which the lock is held by a process.
@@ -33,5 +36,8 @@ def _acquire_lock_and_write_to_cache(cache_location, sleep_interval):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(__doc__)
+        sys.exit(0)
     _acquire_lock_and_write_to_cache(sys.argv[1], float(sys.argv[2]))
 

7d5426df54e5749cc29760c41c52794cc35888f1
Abhidnya <patil.abhidnya@gmail.com>
2020-08-19 15:16:17 -0700

    Adding test

--- tests/test_macos_backend.py
+++ tests/test_macos_backend.py
@@ -6,11 +6,19 @@ import pytest
 import uuid
 import msal
 
+from msal_extensions import KeychainPersistence
+
 if not sys.platform.startswith('darwin'):
     pytest.skip('skipping MacOS-only tests', allow_module_level=True)
 else:
     from msal_extensions.osx import Keychain
-    from msal_extensions.token_cache import OSXTokenCache
+    from msal_extensions.token_cache import OSXTokenCache, PersistedTokenCache
+
+is_running_on_travis_ci = bool(  # (WTF) What-The-Finding:
+    # The bool(...) is necessary, otherwise skipif(...) would treat "true" as
+    # string conditions and then raise an undefined "true" exception.
+    # https://docs.pytest.org/en/latest/historical-notes.html#string-conditions
+    os.getenv("TRAVIS"))
 
 
 def test_keychain_roundtrip():
@@ -41,5 +49,18 @@ def test_osx_token_cache_roundtrip():
         os.utime(cache_file, None)  # Mock having another process update the cache.
         token2 = app.acquire_token_silent(scopes=desired_scopes, account=None)
         assert token1['access_token'] == token2['access_token']
+    finally:
+        shutil.rmtree(test_folder, ignore_errors=True)
+
+@pytest.mark.skipif(is_running_on_travis_ci)
+def test_item_not_found_exception_is_no_op():
+    test_folder = tempfile.mkdtemp(prefix="msal_extension_test_osx_token_cache_roundtrip")
+    cache_file = os.path.join(test_folder, 'msal.cache')
+    open(cache_file, 'w')
+    try:
+        persistence = KeychainPersistence(cache_file, "my_service_name", "my_account_name")
+        app = msal.PublicClientApplication(
+            client_id="client_id", token_cache=PersistedTokenCache(persistence))
+        assert app.get_accounts() == []  # ITEM_NOT_FOUND is handled
     finally:
         shutil.rmtree(test_folder, ignore_errors=True)

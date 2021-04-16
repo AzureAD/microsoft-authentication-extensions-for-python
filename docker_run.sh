bb247451e034b1455dd9f107f25e306d3c773e82
Ray Luo <rayluo.mba@gmail.com>
2021-04-15 19:07:04 -0700

    A docker-based infrastructure to test encryption on Linux easily

---
+++ docker_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+IMAGE_NAME=msal-extensions:latest
+
+docker build -t $IMAGE_NAME - < Dockerfile
+
+echo "After seeing the bash prompt, run the following to test encryption on Linux:"
+echo "    pip install -e ."
+echo "    pytest"
+docker run --rm -it \
+    --privileged \
+    -w /home -v $PWD:/home \
+    $IMAGE_NAME \
+    $1
+

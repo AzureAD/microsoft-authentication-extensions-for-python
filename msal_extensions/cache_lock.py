89a3996a36d5c94efbcd7eca72193b0e98805fe4
Ray Luo <rayluo.mba@gmail.com>
2021-11-11 23:50:04 -0800

    Refactor logs during recent debug effort

--- msal_extensions/cache_lock.py
+++ msal_extensions/cache_lock.py
@@ -43,6 +43,7 @@ class CrossPlatLock(object):
         check_interval = 0.25
         current_time = getattr(time, "monotonic", time.time)
         timeout_end = current_time() + timeout
+        pid = os.getpid()
         while timeout_end > current_time():
             try:
                 with open(self._lockpath, 'x'):  # pylint: disable=unspecified-encoding
@@ -51,15 +52,18 @@ class CrossPlatLock(object):
                 logger.warning("Python 2 does not support atomic creation of file")
                 return False
             except FileExistsError:  # Only Python 3 will reach this clause
-                logger.warning("Lock file exists, trying again after some time")
+                logger.debug(
+                    "Process %d found existing lock file, will retry after %f second",
+                    pid, check_interval)
                 time.sleep(check_interval)
         return False
 
     def __enter__(self):
+        pid = os.getpid()
         if not self._try_to_create_lock_file():
-            logger.warning("Failed to create lock file")
+            logger.warning("Process %d failed to create lock file", pid)
         file_handle = self._lock.__enter__()
-        file_handle.write('{} {}'.format(os.getpid(), sys.argv[0]).encode('utf-8'))
+        file_handle.write('{} {}'.format(pid, sys.argv[0]).encode('utf-8'))
         return file_handle
 
     def __exit__(self, *args):
